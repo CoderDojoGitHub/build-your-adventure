7d5a505396c9ac48fb53e38c741b96f83f2f2bff
Kyle Neath <kneath@gmail.com>
2013-06-21 16:04:30 -0700

    Add some sprites, get the character rendering

--- lessons/2-character/character.js
+++ lessons/2-character/character.js
@@ -1,4 +1,11 @@
 game.addEventListener("load", function(){
 
+  // Get a character
+  character = new Sprite(32, 32)
+  character.image = game.assets['images/boy.gif']
+  character.frame = 0
+  character.x = 0
+  character.y = 0
+  game.rootScene.addChild(character)
 
 })
--- lessons/2-character/lib/game.js
+++ lessons/2-character/lib/game.js
@@ -3,5 +3,5 @@ enchant()
 var game = new Game(240, 240)
 game.scale = 2
 game.fps = 25
-game.preload('images/map.png')
-var map, character
+game.preload('images/map.png', 'images/boy.gif', 'images/girl.gif')
+var map, map, character
