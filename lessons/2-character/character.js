83771f4578344560cb697363d3e7b9d35c65bc26
Kyle Neath <kneath@gmail.com>
2013-06-21 16:09:36 -0700

    Character moves left and right

--- lessons/2-character/character.js
+++ lessons/2-character/character.js
@@ -4,8 +4,24 @@ game.addEventListener("load", function(){
   character = new Sprite(32, 32)
   character.image = game.assets['images/boy.gif']
   character.frame = 0
-  character.x = 0
-  character.y = 0
+  character.x = 100
+  character.y = 100
   game.rootScene.addChild(character)
+
+  function movement(){
+
+    var x = this.x
+
+    if (game.input.left) {
+      x -= 1
+    }
+    if (game.input.right) {
+      x += 1
+    }
+
+    this.x = x
+
+  }
+  character.addEventListener("enterframe", movement)
 
 })
